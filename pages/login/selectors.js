9e562eb1c2c426ce90b788059bccc4cae2f2ae4b
Hardikkumar Diyora <hardikdiyora92@gmail.com>
2019-03-20 13:48:04 +0100

    Revert "Update selectors.js"

--- pages/login/selectors.js
+++ pages/login/selectors.js
@@ -1,6 +1,10 @@
-export default  {
-    usernameInput: '#username',
-    passwordInput: '#password',
-    submitButton: '[type="submit"]',
-    flashMessage: '#flash'
-};
+class Selectors {
+
+    get username(){ return  $('#username'); }
+    get password(){ return $('#password'); }
+    get submit(){ return $('[type="submit"]'); }
+    get flash(){ return  $('#flash'); }
+
+}
+
+export default new Selectors();
